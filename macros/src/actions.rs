1ff03887f7b3dd393c590299ee75606768be2a48
Timothy Meade <zt.tmzt@gmail.com>
2016-10-01 12:48:34 -0400

    Move define_store_action to plugin

--- macros/src/actions.rs
+++ macros/src/actions.rs
@@ -1,23 +1,13 @@
 
 #[macro_export]
-macro_rules! define_store_action {
-    ($w: expr, $store: ident, action $name:ident => ($e:expr)) => (
-        write!($w, "\t\tcase '{}': return {};\n",
-            stringify!($name),
-            format!("{{ {}: {} }}",
-                stringify!($store),
-                format!("(state.{})", stringify!($e))
-            )
-        )
-    )
-}
-
-#[macro_export]
 macro_rules! define_store {
     ($w: expr, store $store:ident {
         default => ($def:expr);
         $(action $act: ident => ($e:expr));*
     }) => ({
+        let mut aliases = String::new();
+        write!(aliases, "\tvar {} = state.{};", stringify!($store), stringify!($store)).unwrap();
+
         let mut actions = String::new();
         $(define_store_action!(actions, $store, action $act => ($e));)*
         write!(actions, "\t\tdefault: return state;\n");
@@ -25,7 +15,7 @@ macro_rules! define_store {
         let default_value = format!("\tstate = state || {{ {}: {} }};\n", stringify!($store), stringify!($def));
         let switch = format!("\tswitch (action.type) {{\n {}\n\t}};", actions);
 
-        let body = format!("\n{}\n{}", default_value, switch);
+        let body = format!("\n{}\n{}\n{}", default_value, aliases, switch);
         let create_store = format!("Redux.createStore(function store_handler_{}(state, action) {{ {} }})",
             stringify!($store), body);
         let store_fn = format!("\nfunction create_store_{}() {{return {}}}", stringify!($store), create_store);
--- plugin/src/actions.rs
+++ plugin/src/actions.rs
@@ -4,6 +4,7 @@ use syntax::ext::base::IdentTT;
 use syntax::parse::token;
 
 use syntax::ast;
+use syntax::ptr::P;
 use syntax::codemap::{Span, DUMMY_SP};
 use syntax::ext::base::{DummyResult, ExtCtxt, MacEager, MacResult, SyntaxExtension, NormalTT, TTMacroExpander};
 use syntax::ext::build::AstBuilder;
@@ -18,6 +19,9 @@ use itertools::Itertools;
 
 pub fn register_store(reg: &mut Registry) {
     // Store definitions
+    reg.register_syntax_extension(token::intern("define_store_action"),
+            NormalTT(Box::new(expand_define_store_action), None, false));
+
 //    reg.register_syntax_extension(token::intern("define_store"),
 //            IdentTT(Box::new(expand_define_store), None, false));
 }
@@ -30,6 +34,26 @@ trait StoreAction {
     
     fn name() -> &'static str;
     fn apply_mut(store: &mut Self::Store);
+}
+
+macro_rules! write_action_case {
+    ($ecx: expr, $w: expr, $store_name:expr, $action_name:expr, $e:expr) => ({
+        let q_store_name = $store_name;
+        let q_action_name = $action_name;
+        let q_expr = $e;
+        let out = $w;
+        quote_stmt!($ecx, {
+            //write!($w, "test");
+            //write!($w, "\t\tcase '{}': return {};\n", "1", "2");
+            write!($out, "\t\tcase '{}': return {};\n",
+                $q_action_name,
+                format!("{{ {}: {} }}",
+                    $q_store_name,
+                    stringify!($q_expr)
+                )
+            )
+        })
+    })
 }
 
 /// TODO: Document
@@ -48,4 +72,53 @@ pub fn expand_define_store<'cx>(ecx: &'cx mut ExtCtxt, span: Span, ident: ast::I
     let stmts = vec![stmt];
     let block = ecx.block(DUMMY_SP, stmts);
     MacEager::expr(ecx.expr_block(block))
+}
+
+fn parse_action_expr<'cx, 'a>(ecx: &'cx mut ExtCtxt, span: Span, parser: &mut Parser<'a>) -> PResult<'a, P<ast::Expr>> {
+    // Expression
+    parser.parse_expr()
+}
+
+fn parse_default_expr<'cx, 'a>(ecx: &'cx mut ExtCtxt, span: Span, parser: &mut Parser<'a>) -> PResult<'a, P<ast::Expr>> {
+    // Expression
+    parser.parse_expr()
+}
+
+fn parse_define_store_action<'cx, 'a>(ecx: &'cx mut ExtCtxt, span: Span, parser: &mut Parser<'a>) -> PResult<'a, Box<MacResult + 'cx>> {
+    // Output stream
+    let w = try!(parser.parse_ident());
+    try!(parser.expect(&token::Comma));
+
+    // Store identifier
+    let store_name = try!(parser.parse_ident()).name.to_string();
+    try!(parser.expect(&token::Comma));
+
+    // Token 'action'
+    try!(parser.parse_ident());
+
+    // Action name
+    let action_name = try!(parser.parse_ident()).name.to_string();
+
+    // =>
+    try!(parser.expect(&token::FatArrow));
+
+    // expression
+    let expr = try!(parse_action_expr(ecx, span, parser));
+
+    let stmt = write_action_case!(ecx, w, store_name, action_name, expr).unwrap();
+
+    let stmts = vec![stmt];
+    let block = ecx.block(DUMMY_SP, stmts);
+    Ok(MacEager::expr(ecx.expr_block(block)))
+}
+
+pub fn expand_define_store_action<'cx>(ecx: &'cx mut ExtCtxt, span: Span, tts: &[TokenTree]) -> Box<MacResult + 'cx> {
+    let mut parser = ecx.new_parser_from_tts(&tts);
+    match parse_define_store_action(ecx, span, &mut parser) {
+        Ok(mac_result) => mac_result,
+        Err(mut err) => {
+            err.emit();
+            DummyResult::any(span)
+        }
+    }
 }
